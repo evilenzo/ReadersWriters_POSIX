c993d47a9af6ffe027d5031a3da4a85d85bb02f7
evilenzo <lingertell@protonmail.com>
2022-02-28 04:01:13 +0300

    Release fixes
    Removed global variables
    
    All comments are now in English
    
    Random improvement

--- main.cpp
+++ main.cpp
@@ -1,206 +1,182 @@
 /*
- * Данная программа реализует схему решения задачи
- * "читатели-писатели", используя стандарт POSIX.
+ * Readers-Writers problem solution using POSIX API
  *
- * Приоритет писателя.
- * "Как только появился хоть один писатель, никого больше не пускать.
- * Все остальные могут простаивать"
+ * Writers-preference.
+ * "No writer, once added to the queue, shall be kept waiting
+ * longer than absolutely necessary."
  *
- * Количество читателей, писателей и повторений, задержка, а также возможность
- * случайно генерировать задержку (добавляя случайное число от 0-3
- * к установленной) чтения/записи устанавливаются директивами препроцессора.
+ * Readers and writers amount, read/write delay
+ * or random delay generation option (adding 0-3 to user-defined)
+ * can be changed by #define directives
+ *
+ * P.S. #define directives was one of conditions of this task
  */
 
 
+#define READERS_AMOUNT 5
+#define WRITERS_AMOUNT 3
 
-#define READERS_NUM 5  // Количество читателей
-#define WRITERS_NUM 3  // Количество писателей
+#define REPEATS_AMOUNT 3
 
-#define NUM_OF_REPEATS 3  // Количество повторений
+#define READ_DELAY 2
+#define WRITE_DELAY 3
 
-#define READ_DELAY 2   // Задержка чтения
-#define WRITE_DELAY 3  // Задержка записи
-
-#define RANDOMIZE_DELAY false  // Рандомизировать задержку
-
+#define RANDOMIZE_DELAY true  // Randomly add 0-3 to upper values
 
 
-#include <iostream>
+
 #include <pthread.h>
 #include <unistd.h>
 
+#include <iostream>
+#include <random>
 
-// Mutex для читателей
-struct ReadMutex {
-    pthread_mutex_t m = {};
 
-    ReadMutex() { pthread_mutex_init(&m, nullptr); }
-    ~ReadMutex() { pthread_mutex_destroy(&m); }
-} read_mutex;
+template <typename T>
+struct SyncStorage;
 
-// Mutex для писателей
-struct WriteMutex {
-    pthread_mutex_t m = {};
+template <>
+struct SyncStorage<void> {
+  mutable pthread_mutex_t m = {};
 
-    int a;
+  SyncStorage() { pthread_mutex_init(&m, nullptr); }
+  ~SyncStorage() { pthread_mutex_destroy(&m); }
+};
 
-    WriteMutex() { pthread_mutex_init(&m, nullptr); }
-    ~WriteMutex() { pthread_mutex_destroy(&m); }
-} write_mutex;
+template <typename T>
+struct SyncStorage : SyncStorage<void> {
+  T value = {};
+};
 
-// Mutex для вывода
-struct OutputMutex {
-    pthread_mutex_t m = {};
+struct ReadWriteSync {
+  explicit ReadWriteSync(uint32_t value_) : value(value_) {}
+  uint32_t value;
 
-    OutputMutex() { pthread_mutex_init(&m, nullptr); }
-    ~OutputMutex() { pthread_mutex_destroy(&m); }
-} output_mutex;
+  SyncStorage<void> read;
+  SyncStorage<void> write;
+  SyncStorage<size_t> readers_count;
+  SyncStorage<size_t> writers_count;
 
-// Количество читателей
-struct ReadersCount {
-    int i = 0;
-    pthread_mutex_t m = {};
-
-    ReadersCount() { pthread_mutex_init(&m, nullptr); }
-    ~ReadersCount() { pthread_mutex_destroy(&m); }
-} readers_count;
-
-// Количество писателей
-struct WritersCount {
-    int i = 0;
-    pthread_mutex_t m = {};
-
-    WritersCount() { pthread_mutex_init(&m, nullptr); }
-    ~WritersCount() { pthread_mutex_destroy(&m); }
-} writers_count;
+  SyncStorage<void> cout;
+};
 
 void* reader(void* args) {
-    const int* resource = (int*)args;
+  const auto resource = reinterpret_cast<ReadWriteSync*>(args);
 
-    int repeats_num = NUM_OF_REPEATS;
+  uint32_t repeats_amount = REPEATS_AMOUNT;
 
-    while (repeats_num > 0) {
-        // Блокируем mutex чтения и количества читателей
-        pthread_mutex_lock(&read_mutex.m);
-        pthread_mutex_lock(&readers_count.m);
+  std::random_device r;
+  std::mt19937 eng{r()};
+  std::uniform_int_distribution<int> dist(0, 3);
 
-        // Добавляем себя в читатели
-        ++readers_count.i;
-        if (readers_count.i == 1) {
-            // Блокируем mutex на запись
-            pthread_mutex_lock(&write_mutex.m);
-        }
+  while (repeats_amount > 0) {
+    // Adding ourselves to readers and locking write
+    pthread_mutex_lock(&resource->read.m);
+    pthread_mutex_lock(&resource->readers_count.m);
 
-        // Разблокируем mutex чтения и количества читателей
-        pthread_mutex_unlock(&readers_count.m);
-        pthread_mutex_unlock(&read_mutex.m);
-
-        // Читаем и выводим
-        pthread_mutex_lock(&output_mutex.m);
-        std::cout << *resource << std::endl;
-        pthread_mutex_unlock(&output_mutex.m);
-
-        // Блокируем mutex количества читателей
-        pthread_mutex_lock(&readers_count.m);
-
-        // Убираем себя из читателей
-        --readers_count.i;
-        if (readers_count.i == 0) {
-            // Читателей нет, разрешаю писать
-            pthread_mutex_unlock(&write_mutex.m);
-        }
-
-        // Разблокируем mutex количества читателей
-        pthread_mutex_unlock(&readers_count.m);
-
-        // Ожидаем
-        int randomized = 0;
-
-        if (RANDOMIZE_DELAY) {
-            randomized = rand() % 4;
-        }
-
-        sleep(READ_DELAY + randomized);
-
-        --repeats_num;
+    ++resource->readers_count.value;
+    if (resource->readers_count.value == 1) {
+      pthread_mutex_lock(&resource->write.m);
     }
 
-    return nullptr;
+    pthread_mutex_unlock(&resource->readers_count.m);
+    pthread_mutex_unlock(&resource->read.m);
+
+
+    // Read and output
+    pthread_mutex_lock(&resource->cout.m);
+    std::cout << resource->value << std::endl;
+    pthread_mutex_unlock(&resource->cout.m);
+
+
+    // Remove ourselves from readers and return access for writing if no more
+    // readers exist
+    pthread_mutex_lock(&resource->readers_count.m);
+
+    --resource->readers_count.value;
+    if (resource->readers_count.value == 0) {
+      pthread_mutex_unlock(&resource->write.m);
+    }
+
+    pthread_mutex_unlock(&resource->readers_count.m);
+
+
+    // Delay
+    size_t randomized = 0;
+    if (RANDOMIZE_DELAY) {
+      randomized = dist(eng);
+    }
+    sleep(READ_DELAY + randomized);
+    --repeats_amount;
+  }
+
+  return nullptr;
 }
 
 void* writer(void* args) {
-    int* resource = (int*)args;
+  const auto resource = reinterpret_cast<ReadWriteSync*>(args);
 
-    int repeats_num = NUM_OF_REPEATS;
+  uint32_t repeats_amount = REPEATS_AMOUNT;
 
-    while (repeats_num > 0) {
-        // Блокируем mutex количества писателей
-        pthread_mutex_lock(&writers_count.m);
+  std::random_device r;
+  std::mt19937 eng{r()};
+  std::uniform_int_distribution<int> dist(0, 3);
 
-        // Добавляем себя в писатели
-        ++writers_count.i;
-        if (writers_count.i == 1) {
-            // Блокируем мьютекс на чтение
-            pthread_mutex_lock(&read_mutex.m);
-        }
+  while (repeats_amount > 0) {
+    // Adding ourselves to writers and locking read
+    pthread_mutex_lock(&resource->writers_count.m);
 
-        // Разблокируем mutex количества писателей
-        pthread_mutex_unlock(&writers_count.m);
-
-        // Блокируем mutex на запись
-        pthread_mutex_lock(&write_mutex.m);
-
-        // Пишем
-        (*resource)++;
-
-        // Разблокируем mutex на запись
-        pthread_mutex_unlock(&write_mutex.m);
-
-        // Блокируем mutex количества писателей
-        pthread_mutex_lock(&writers_count.m);
-
-        // Убираем себя из писателей
-        --writers_count.i;
-        if (writers_count.i == 0) {
-            // Писателей нет, разрешаю читать
-            pthread_mutex_unlock(&read_mutex.m);
-        }
-
-        // Разблокируем mutex количества писателей
-        pthread_mutex_unlock(&writers_count.m);
-
-        // Ожидаем
-        int randomized = 0;
-
-        if (RANDOMIZE_DELAY) {
-            randomized = rand() % 4;
-        }
-
-        sleep(WRITE_DELAY + randomized);
-
-        --repeats_num;
+    ++resource->writers_count.value;
+    if (resource->writers_count.value == 1) {
+      pthread_mutex_lock(&resource->read.m);
     }
 
-    return nullptr;
+    pthread_mutex_unlock(&resource->writers_count.m);
+
+    // Writing
+    pthread_mutex_lock(&resource->write.m);
+    ++resource->value;
+    pthread_mutex_unlock(&resource->write.m);
+
+
+    // Remove ourselves from writers and return access for reading if no more
+    // writers exist
+    pthread_mutex_lock(&resource->writers_count.m);
+
+    --resource->writers_count.value;
+    if (resource->writers_count.value == 0) {
+      pthread_mutex_unlock(&resource->read.m);
+    }
+
+    pthread_mutex_unlock(&resource->writers_count.m);
+
+
+    // Delay
+    int randomized = 0;
+    if (RANDOMIZE_DELAY) {
+      randomized = dist(eng);
+    }
+    sleep(WRITE_DELAY + randomized);
+    --repeats_amount;
+  }
+
+  return nullptr;
 }
 
 int main() {
-    /// Общий ресурс для чтения-записи
-    int resource = 1;
+  ReadWriteSync resource{1};
+  pthread_t threads[READERS_AMOUNT + WRITERS_AMOUNT];
+  size_t i = 0;
 
-    pthread_t threads[READERS_NUM + WRITERS_NUM];
+  for (; i < READERS_AMOUNT; ++i) {
+    pthread_create(&threads[i], nullptr, reader, &resource);
+  }
 
-    std::size_t i = 0;
+  for (int j = 0; j < WRITERS_AMOUNT; ++j, ++i) {
+    pthread_create(&threads[i], nullptr, writer, &resource);
+  }
 
-    for (; i < READERS_NUM; ++i) {
-        pthread_create(&threads[i], NULL, reader, &resource);
-    }
-
-    for (int j = 0; j < WRITERS_NUM; ++j, ++i) {
-        pthread_create(&threads[i], NULL, writer, &resource);
-    }
-
-    for (i = 0; i < READERS_NUM + WRITERS_NUM; ++i) {
-        pthread_join(threads[i], nullptr);
-    }
+  for (i = 0; i < READERS_AMOUNT + WRITERS_AMOUNT; ++i) {
+    pthread_join(threads[i], nullptr);
+  }
 }
